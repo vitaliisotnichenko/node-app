301917edad93a5d57199a6cc28b0770326da1656
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2021-04-28 00:01:35 +0200

    requests and sending responses

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const server = http.createServer( (req, resp) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    resp.setHeader('Content-Type', 'text/html');
+    resp.write('<html>Hello</html>');
+    resp.end();
 });
 
 server.listen(3000);
