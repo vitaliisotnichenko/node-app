a1581e38127fbbeec03716854c7e31d5663dd7f8
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2021-04-28 22:28:56 +0200

    redirecting requests

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer( (req, resp) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         resp.write('<html>Enter Message</html>');
         resp.write('<form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form>');
         return resp.end();
     }
+    if(url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Dummy')
+        resp.writeHead(302, {'Location': '/'})
+        return resp.end()
+    }
+
     resp.setHeader('Content-Type', 'text/html');
     resp.write('<html>Hello</html>');
     resp.end();
