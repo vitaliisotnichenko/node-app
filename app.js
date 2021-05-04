5150953142639c3ce3601412178b09b9a54698b1
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2021-05-05 00:28:14 +0200

    add routes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next)=> {
-    console.log('In the middleware');
-    next();
+app.use('/add-product', (req, res, next)=> {
+    console.log('In the next middleware');
+    res.send('<h1>Add product</h1>');
 })
-app.use((req, res, next)=> {
+
+app.use('/', (req, res, next)=> {
     console.log('In the next middleware');
     res.send('<h1>Hello from Express</h1>');
 })
