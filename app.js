81cae983ea61c5487d7187e4e813273620aab4d1
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2021-05-06 23:57:12 +0200

    filtering paths

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(customerRoutes);
 app.use((req, res, next) => {
     res.status(404).send('<h1>Page not found</h1>')
