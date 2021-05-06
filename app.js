a47c0d17bb90fe178fe4c2dc6ca70156dd498ad7
Vitalii Sotnichenko <vitalii.sotnichenko@gmail.com>
2021-05-06 23:43:22 +0200

    using express router

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const app = express();
+const adminRoutes = require('./routes/admin')
+const customerRoutes = require('./routes/shop')
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (req, res, next)=> {
-    console.log('In the next middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title="><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next)=> {
-    console.log('In the next middleware');
-    res.send('<h1>Hello from Express</h1>');
-})
+app.use(adminRoutes);
+app.use(customerRoutes);
 
 app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res, next)=> {
+    console.log('In the next middleware');
+    res.send('<h1>Hello from Express</h1>');
+})
+
+module.exports = router;
